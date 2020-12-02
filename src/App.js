48fdf8c0b9b51a8d5e5e26bea2ea6dbaecbe047a
Gian <gianlu_009@hotmail.com>
2020-12-01 21:25:11 -0300

    Modificaciones iniciales al React Sample

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sitio en construcción
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h5>
+          Mientras tanto, visitanos en <span class="underline">
+            <a
+            className="App-link"
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+                >Instagram!
+            </a>
+            </span>
+        </h5>
       </header>
     </div>
   );
